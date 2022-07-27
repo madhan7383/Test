08f0e3dffbc342cdf829b2d93a4530ae6d91b497
madhan7383 <110127685+madhan7383@users.noreply.github.com>
2022-07-28 00:42:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <img src="./logo.png" width="40px" />
+      <Text>Madhan</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
